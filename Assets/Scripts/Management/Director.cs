3569061bfbe9e59e9e2175562f33a8f406d97f29
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-07-11 10:43:07 -0300

    - Added director and manager system

---
+++ Assets/Scripts/Management/Director.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class Director
+{
+    private static Dictionary<string, Manager> managers = new Dictionary<string, Manager>();
+    [RuntimeInitializeOnLoadMethod]
+    public static void Initialize()
+    {
+        managers = new Dictionary<string, Manager>();
+        SceneManager.LoadScene("Managers", LoadSceneMode.Additive);
+
+    }
+
+    public static T GetManager<T>() where T : Manager
+    {
+        string type = typeof(T).ToString();
+        if (managers != null && managers.ContainsKey(type) && managers[type] != null)
+        {
+            return managers[type] as T;
+        }
+        Debug.LogWarning("Manager type " + type + " not found, returning null.");
+        return null;
+    }
+
+    public static void SubscribeManager<T>(T manager) where T : Manager
+    {
+        managers[typeof(T).ToString()] = manager;
+    }
+
+}
---
+++ Assets/Scripts/Management/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Manager : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SubscribeToDirector();
+    }
+
+    protected abstract void SubscribeToDirector();
+
+}
