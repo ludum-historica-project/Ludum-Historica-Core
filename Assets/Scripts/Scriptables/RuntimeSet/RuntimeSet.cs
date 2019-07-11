def40931ba6f1415d66b9ca3fad9aa23a9af200b
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-07-11 10:44:55 -0300

    - Added RuntimeSets and RuntimeSetSubscribers

---
+++ Assets/Scripts/Scriptables/RuntimeSet/RuntimeSet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Scriptables/Runtime Set")]
+public class RuntimeSet : ScriptableObject
+{
+    public List<GameObject> objects = new List<GameObject>();
+
+    public void SubscribeObject(GameObject go)
+    {
+        if (!objects.Contains(go)) objects.Add(go);
+    }
+
+    public void UnSubscribeObject(GameObject go)
+    {
+        objects.Remove(go);
+    }
+}
---
+++ Assets/Scripts/Scriptables/RuntimeSet/RuntimeSetSubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeSetSubscriber : MonoBehaviour
+{
+    public RuntimeSet targetSet;
+    private void Awake()
+    {
+        if (targetSet == null)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnEnable()
+    {
+        targetSet.SubscribeObject(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        targetSet.UnSubscribeObject(gameObject);
+    }
+}
