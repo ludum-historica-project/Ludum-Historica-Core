1bbfa001e1f7be23636f678383fdcdb111304954
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-07-11 10:44:11 -0300

    - Added Pause Manager

---
+++ Assets/Scripts/Management/PauseManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : Manager
+{
+    protected override void SubscribeToDirector()
+    {
+        Director.SubscribeManager(this);
+    }
+
+    private bool _paused;
+    public bool Pause
+    {
+        get
+        {
+            return _paused;
+        }
+        set
+        {
+            if (_paused != value)
+            {
+                _paused = value;
+
+                Time.timeScale = _paused ? 0 : 1;
+                Time.fixedDeltaTime = Time.timeScale * 0.02f;
+            }
+        }
+    }
+}
