b9dd447fa0f5eea36e7b04c0b7a98d61e77affc1
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-07-11 10:45:33 -0300

    - Added PlayerPrefsManager - Added SaveManager

---
+++ Assets/Scripts/Management/PlayerPrefsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : Manager
+{
+    protected override void SubscribeToDirector()
+    {
+        Director.SubscribeManager(this);
+    }
+    public void SaveToPlayerPrefs(string key, object data)
+    {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(data));
+    }
+    public T LoadFromPlayerPrefs<T>(string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            JsonUtility.FromJson<T>(PlayerPrefs.GetString(key));
+        }
+        Debug.LogWarning("Key " + key + " doesn't exist in the PlayerPrefs, returning null.");
+        return default(T);
+    }
+
+    public void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ Assets/Scripts/Management/SaveManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveManager : Manager
+{
+    protected override void SubscribeToDirector()
+    {
+        Director.SubscribeManager(this);
+    }
+
+    public List<ScriptableObject> saveableObjects = new List<ScriptableObject>();
+
+    public void SaveData(int slot = 0)
+    {
+        Dictionary<string, string> jsonDictionary = new Dictionary<string, string>();
+        foreach (var saveableObject in saveableObjects)
+        {
+            jsonDictionary[saveableObject.name] = JsonUtility.ToJson(saveableObject);
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        if (!Directory.Exists(Application.persistentDataPath + "/saves"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saves");
+        }
+        FileStream file = File.Create(Application.persistentDataPath + "/saves/save" + slot + ".save");
+        bf.Serialize(file, jsonDictionary);
+        file.Close();
+    }
+
+    public void LoadData(int slot = 0)
+    {
+        if (File.Exists(Application.persistentDataPath + "/saves/save" + slot + ".save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/saves/save" + slot + ".save", FileMode.Open);
+            Dictionary<string, string> data = (Dictionary<string, string>)bf.Deserialize(file);
+            file.Close();
+
+            foreach (var saveableObject in saveableObjects)
+            {
+                if (data.ContainsKey(saveableObject.name))
+                {
+                    JsonUtility.FromJsonOverwrite(data[saveableObject.name], saveableObject);
+                }
+                else
+                {
+                    Debug.LogWarning(string.Format("[SaveManager] No data for {0} found in save slot {1}.", saveableObject.name, slot));
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No save file found at slot " + slot);
+        }
+    }
+}
