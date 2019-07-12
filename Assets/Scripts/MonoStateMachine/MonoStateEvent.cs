8c363050202bc641fd1fec7d3a003ac169409213
ilasaigues <32438118+ilasaigues@users.noreply.github.com>
2019-07-12 09:49:44 -0300

    - Added MonoStateMachine

---
+++ Assets/Scripts/MonoStateMachine/MonoStateEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[System.Serializable]
+public class MonoStateEvent
+{
+    public List<GameEvent> gameEvents = new List<GameEvent>();
+    public UnityEvent onRaiseEvent;
+    public void Raise()
+    {
+        for (int i = gameEvents.Count - 1; i >= 0; i--)
+        {
+            gameEvents[i].Raise();
+        }
+        onRaiseEvent.Invoke();
+    }
+}
---
+++ Assets/Scripts/MonoStateMachine/MonoStateMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoStateMachine : MonoBehaviour
+{
+    public MonoState _currentState;
+
+    public void ChangeState(MonoState newState)
+    {
+        if (_currentState == newState) return;
+        if (_currentState != null) _currentState.transitionOut.Raise();
+        _currentState = newState;
+        _currentState.transitionIn.Raise();
+    }
+
+    public void Update()
+    {
+        if (_currentState != null) _currentState.update.Raise();
+    }
+}
